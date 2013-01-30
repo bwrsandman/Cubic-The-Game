5ba28c9e239d3e5cdac7394b2008947a01e76c4b
Sandy Carter <bwrsandman@gmail.com>
2013-01-30 03:53:40 -0500

    Updating the Cubic Game

--- Cubic-The-Game/CubicGame.cs
+++ Cubic-The-Game/CubicGame.cs
@@ -1,14 +1,11 @@
-#region using
+#region Using Statements
 using System;
-using System.Collections.Generic;
-using System.Linq;
+
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
+
 #endregion
 
 namespace Cubic_The_Game
@@ -19,10 +16,8 @@ namespace Cubic_The_Game
     public class CubicGame : Microsoft.Xna.Framework.Game
     {
         #region Fields
-
         GraphicsDeviceManager graphics;
         ScreenManager screenManager;
-
 
         // By preloading any assets used by UI rendering, we avoid framerate glitches
         // when they suddenly need to be loaded in the middle of a menu transition.
@@ -30,23 +25,16 @@ namespace Cubic_The_Game
         {
             "gradient",
         };
-
-        
         #endregion
 
         #region Initialization
-
-
         /// <summary>
         /// The main game constructor.
         /// </summary>
         public CubicGame()
         {
             Content.RootDirectory = "Content";
-
             graphics = new GraphicsDeviceManager(this);
-            //graphics.PreferredBackBufferWidth = 853;
-            //graphics.PreferredBackBufferHeight = 480;
             graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
             graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
             graphics.IsFullScreen = true;
@@ -62,9 +50,22 @@ namespace Cubic_The_Game
             screenManager.AddScreen(new SplashScreen(), null);
         }
 
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+
+        }
 
         /// <summary>
-        /// Loads graphics content.
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
         /// </summary>
         protected override void LoadContent()
         {
@@ -73,13 +74,25 @@ namespace Cubic_The_Game
                 Content.Load<object>(asset);
             }
         }
-
-
         #endregion
 
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update (GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
+                Exit();
+            }
+            // TODO: Add your update logic here
+            base.Update(gameTime);
+        }
+
         #region Draw
-
-
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -90,8 +103,6 @@ namespace Cubic_The_Game
             // The real drawing happens inside the screen manager component.
             base.Draw(gameTime);
         }
-
-
         #endregion
     }
 }
